1018b81218d6ad8854de88f75e0ee47d737208fe
ncristovao <cristovao.candido.neto@ccc.ufcg.edu.br>
2025-05-14 22:18:42 -0300

    add sample data from real execution

--- clone-test-repositories.sh
+++ clone-test-repositories.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mkdir -p workstation
 cd workstation
-git clone https://github.com/ZJU-ACES-ISE/chatunitest-maven-plugin
-git clone https://github.com/syqu22/spring-boot-shop-sample
+git clone https://github.com/green-code-initiative/creedengo-python
