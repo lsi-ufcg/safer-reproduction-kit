7e8b08ac0315126bcca49870ffbb11b4c72b2f42
ncristovao <cristovao.candido.neto@ccc.ufcg.edu.br>
2025-05-16 12:59:22 -0300

    add maven results

--- clone-test-repositories.sh
+++ clone-test-repositories.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 mkdir -p workstation
 cd workstation
-git clone https://github.com/google/gson.git
-git clone https://github.com/green-code-initiative/creedengo-python
+git clone https://github.com/loks666/get_jobs.git
+git clone https://github.com/microcks/microcks-testcontainers-java.git
+git clone https://github.com/medblocks/openFHIR.git
+git clone https://github.com/rabbitmq/rabbitmq-amqp-java-client.git
+git clone https://github.com/EntrevistadorInteligente/si-orchestrator.git
+git clone https://github.com/centreformicrosimulation/SimPaths.git
+git clone https://github.com/lhccong/sql-dog-backend.git
+git clone https://github.com/vert-x3/vertx-rabbitmq-client.git
+git clone https://github.com/Cloud-Solutions-International/antikythera.git
+git clone https://github.com/Rapter1990/todowithcouchbase.git
+git clone https://github.com/jeddict/jeddict-ai.git
