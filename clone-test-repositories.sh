b6c98d712ce61541cb51a4b41613bef48ea4ca49
ncristovao <cristovao.candido.neto@ccc.ufcg.edu.br>
2025-05-19 19:31:59 -0300

    add maven results

--- clone-test-repositories.sh
+++ clone-test-repositories.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 mkdir -p workstation
 cd workstation
+
+# Maven
+
+git clone https://github.com/green-code-initiative/creedengo-python.git
+git clone https://github.com/Dddddduo/BalloonWords.git
+git clone https://github.com/spring-petclinic/spring-petclinic-langchain4j.git
+git clone https://github.com/spring-petclinic/spring-petclinic-ai.git
+git clone https://github.com/Sigma-Skidder-Team/SigmaRebase.git
+git clone https://github.com/datafusion-contrib/datafusion-sqlancer.git
+git clone https://github.com/ollama4j/ollama4j.git
 git clone https://github.com/loks666/get_jobs.git
 git clone https://github.com/microcks/microcks-testcontainers-java.git
 git clone https://github.com/medblocks/openFHIR.git
@@ -12,3 +22,10 @@ git clone https://github.com/vert-x3/vertx-rabbitmq-client.git
 git clone https://github.com/Cloud-Solutions-International/antikythera.git
 git clone https://github.com/Rapter1990/todowithcouchbase.git
 git clone https://github.com/jeddict/jeddict-ai.git
+git clone https://github.com/imi-ms/MoPat.git
+git clone https://github.com/mabartos/keycloak-adaptive-authn.git
+git clone https://github.com/Rapter1990/parkinglot.git
+git clone https://github.com/facebookincubator/CP4M.git
+git clone https://github.com/ajaynegi45/LibraryMan-API.git
+git clone https://github.com/ZJU-ACES-ISE/chatunitest-maven-plugin.git
+git clone https://github.com/syqu22/spring-boot-shop-sample.git
