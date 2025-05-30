c2f298487e2f72e4d0c75f34cf1e1a2592b9da45
ncristovao <cristovao.candido.neto@ccc.ufcg.edu.br>
2025-05-30 14:27:03 -0300

    create github artifacts from dataset results

---
+++ create-github-artifacts.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+pretty_print() {
+  local DEFAULT="\e[0m"
+  local RED="\e[1;31m"
+  local YELLOW="\e[1;33m"
+  local BLUE="\e[1;34m"
+  local GREEN="\e[1;92m"
+
+  local color_uppercase="${1^^}"
+  local message="$2"
+  echo -e "${!color_uppercase}${message}${DEFAULT}"
+}
+
+project_base_path=$(pwd)/workstation
+dataset=results/dataset.csv
+
+# Read the Dataset
+tail -n +2 $dataset | while IFS=',' read -r c1 project_name c3 c4 c5 c6 c7 c8 c9 c10 c11 c12 c13 c14 type c16; do
+  pretty_print blue "Creating github artifacts for $project_name"
+  project_root_path=$project_base_path/$type/$project_name
+
+  file = ""
+  if [ "$type" == "maven" ]; then
+    file="pom.xml"
+  elif [ "$type" == "gradle" ]; then
+    file="build.gradle"
+  else
+    echo "Unknown project type" >&2
+    continue
+  fi
+
+  ./bash/commit-dependencies-file.sh $project_root_path $file
+  ./bash/submit-artifacts-github.sh $project_root_path $(pwd)/outputs/stdout/$project_name.txt
+
+done
