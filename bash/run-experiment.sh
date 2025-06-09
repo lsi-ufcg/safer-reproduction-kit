9c26e549acf75618f84d931494ece0201ffbc508
ncristovao <cristovao.candido.neto@ccc.ufcg.edu.br>
2025-06-09 20:30:57 -0300

    stop creating log files

--- bash/run-experiment.sh
+++ bash/run-experiment.sh
@@ -23,13 +23,13 @@ echo "Running safer for: $project_path, See outputs/stdout/$project_name.txt"
 cd safer/src
 
 start_time=$(date +%s)
-PROJECT_ROOT_PATH="$project_root_path" npx tsx script.ts > ../../outputs/stdout/$project_name.txt 2> ../../outputs/stderr/$project_name.txt
+output=$(PROJECT_ROOT_PATH="$project_root_path" npx tsx script.ts | tee /dev/tty)
 cd ../../
 
 end_time=$(date +%s)
 execution_time=$((end_time - start_time))
 
-csv_line=$(cat outputs/stdout/$project_name.txt | grep -A2 '^CSV:' | tail -n1)
+csv_line=$(echo "$output" | grep -A2 '^CSV:' | tail -n1)
 
 if [ -z "$csv_line" ]; then
   pretty_print red "Safer failed to execute in the project $project_name.\nSee outputs/stderr/$project_name.txt" >&2
