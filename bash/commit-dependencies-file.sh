9ce4bb5989d488e98ff6e4460e375b451f4abc55
ncristovao <cristovao.candido.neto@ccc.ufcg.edu.br>
2025-06-16 22:58:12 -0300

    fix pr creation

--- bash/commit-dependencies-file.sh
+++ bash/commit-dependencies-file.sh
@@ -10,5 +10,5 @@ git checkout safer-result 2>/dev/null || git checkout -b safer-result
 git add $dependencies_file
 
 git commit -m "add new $dependencies_file updated by Safer" 2> /dev/null
-git push -u origin safer-result
+git push --force -u origin safer-result
 git checkout $main_branch
